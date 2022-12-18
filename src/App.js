98c50db2a50536f6df986506c65f175daf83dc85
Ezequiel <ezequielferreras2@gmail.com>
2022-12-18 11:19:14 -0400

    asss

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ var [isNavbarHidden, setIsNavbarHidden] = useState(false);
  // not-authenticated //checking //authenticated
 const usuario ={
 
-  rol:"user",
+  rol:"user   ",
   status:"not-authenticated"
 
 }
