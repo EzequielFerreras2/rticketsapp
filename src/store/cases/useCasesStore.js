019d85c9dc6688e1fed64a2c94919b0d73c2022d
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2023-01-25 16:45:46 -0400

    changes.

--- src/store/cases/useCasesStore.js
+++ src/store/cases/useCasesStore.js
@@ -1,3 +1,45 @@
 import {useSelector,useDispatch} from 'react-redux'
 import Swal from 'sweetalert2';
 import  rticketsApp from '../../api/RticketsAppApi'
+import {getCase,getCases,getCasesByUser} from '../cases/casesSlice'
+
+export const useCasesStore = () => {
+
+    const { Case,Cases,CasesByUser } =  useSelector( state => state.cases );
+    const dispatch = useDispatch();
+
+    const onGetCases =async() =>{
+
+        try {
+            const {data} = await rticketsApp.get('/cases/subcategory');
+            const {cases} = data;
+            dispatch(getCases(cases));
+        } 
+        catch ({response})
+         {
+            const{data} = response;
+            if(data.ok === false)
+            {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: `${data.msg}.!!!`,
+                  })
+            };
+            }
+            
+    };
+
+  return {
+
+     //Propieties
+     Case,
+     Cases,
+     CasesByUser,
+     //Methos
+     onGetCases,
+    
+  };
+}
+
+ 
