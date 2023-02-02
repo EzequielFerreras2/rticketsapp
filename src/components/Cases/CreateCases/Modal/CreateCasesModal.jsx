95a01f60c6850e6888b7140f85aeb25f88670b4b
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2023-02-02 14:53:21 -0400

    Changes.

--- src/components/Cases/CreateCases/Modal/CreateCasesModal.jsx
+++ src/components/Cases/CreateCases/Modal/CreateCasesModal.jsx
@@ -18,162 +18,173 @@ const CreateCasesModal = ({open,onClose}) => {
     const {CategoryCasesBySubCategory,onGetCategoryCasesBySubCategory}= useCategoryCasesStore()
     
 
-const [categoryS, setCateogryS] = useState("")
-const handleSelectCategoryChange = (event) => {setCateogryS(event.target.value); setSubCateogryS(""); setCateogryCaseS("")};
+    const [categoryS, setCateogryS] = useState("")
+    const handleSelectCategoryChange = (event) => {setCateogryS(event.target.value); setSubCateogryS(""); setCateogryCaseS("")};
 
-const [subCategoryS, setSubCateogryS] = useState("")
-const handleSelectSubCategoryChange = (event) => {setSubCateogryS(event.target.value); setCateogryCaseS("")};
+    const [subCategoryS, setSubCateogryS] = useState("")
+    const handleSelectSubCategoryChange = (event) => {setSubCateogryS(event.target.value); setCateogryCaseS("")};
 
-const [categoryCaseS, setCateogryCaseS] = useState("")
-const handleSelectCategoryCaseChange = (event) => {setCateogryCaseS(event.target.value); };
+    const [categoryCaseS, setCateogryCaseS] = useState("")
+    const handleSelectCategoryCaseChange = (event) => {setCateogryCaseS(event.target.value); };
 
-const isDisableSubCategory= useMemo( ()=> SubCategoryByCategory.length === 0, [SubCategoryByCategory]);
-const isDisableCategoryCase= useMemo( ()=> CategoryCasesBySubCategory.length === 0, [CategoryCasesBySubCategory]);
+    const isDisableSubCategory= useMemo( ()=> SubCategoryByCategory.length === 0, [SubCategoryByCategory]);
+    const isDisableCategoryCase= useMemo( ()=> CategoryCasesBySubCategory.length === 0, [CategoryCasesBySubCategory]);
 
+    const modalClose =()=>{
 
+        setCateogryCaseS("");
+        setCateogryS("");
+        setSubCateogryS("");
+    }
 
-useEffect(() => {
-    ongetCategory();
-}, []);
+    useEffect(() => {
+        ongetCategory();
+    }, []);
 
-useEffect(() => {
-    onGetSubCategoryByCategory(categoryS);
-}, [categoryS]);
+    useEffect(() => {
 
-useEffect(() => {
-    onGetCategoryCasesBySubCategory(subCategoryS);
-}, [subCategoryS]);
-
-
-
-
-//Estilo del Modal
-const modalStyles = {
-    inputFields: {
-        display: 'flex',
-        flexDirection: 'column',
-        marginTop: '20px',
-        marginBottom: '15px',
-        '.MuiFormControl-root': {
-            marginBottom: '20px',
-        },
-    },
-};
-
-const validationSchema = Yup.object().shape({
-    category: Yup.string().required('Campo requerido')  
-});
-
-//useForm
-const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-} = useForm({
-    resolver: yupResolver(validationSchema)
-});
-
-const saveChanges = (data) => {
-
-};
-
-
-const getContent =()=>(
-<Box sx={modalStyles.inputFields}>
-
-        <Typography>
-            Tipo de Caso:
-        </Typography>
-        <hr/>
-    <Grid container spacing={3}>
+            onGetSubCategoryByCategory(categoryS);
         
-            <Grid item xs={4}>
+        
+    }, [categoryS]);
+
+    useEffect(() => {
+      
+            onGetCategoryCasesBySubCategory(subCategoryS);
+        
+        
+    }, [subCategoryS]);
+
+
+
+
+    //Estilo del Modal
+    const modalStyles = {
+        inputFields: {
+            display: 'flex',
+            flexDirection: 'column',
+            marginTop: '20px',
+            marginBottom: '15px',
+            '.MuiFormControl-root': {
+                marginBottom: '20px',
+            },
+        },
+    };
+
+    const validationSchema = Yup.object().shape({
+        category: Yup.string().required('Campo requerido')  
+    });
+
+    //useForm
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(validationSchema)
+    });
+
+    const saveChanges = (data) => {
+
+    };
+
+
+    const getContent =()=>(
+    <Box sx={modalStyles.inputFields}>
+
+            <Typography>
+                Tipo de Caso:
+            </Typography>
+            <hr/>
+        <Grid container spacing={3}>
+            
+                <Grid item xs={4}>
+                    
+                    <FormControl fullWidth>
+                            <InputLabel id="category">Categoria</InputLabel>
+                            <Select
+
+                                labelId="category"
+                                id="category"
+                                value={categoryS}
+                                {...register("category")}
+                                label="Categoria"
+                                onChange={handleSelectCategoryChange}
+                                error={!!errors.company}
+                                
+                                
+                            >
+                            {
+                                Category.map((category)=>{
+                                return (
+                                    <MenuItem key={category.id} value={category.id} >{category.category} </MenuItem>
+                                );
+                                })
+                                
+                            }; 
+                            </Select>
+                        </FormControl>
+
+                        
+                        
                 
+                </Grid>
+
+                <Grid item xs={4}>
                 <FormControl fullWidth>
-                        <InputLabel id="category">Categoria</InputLabel>
-                        <Select
+                            <InputLabel id="subCategory">SubCategoria</InputLabel>
+                            <Select
 
-                            labelId="category"
-                            id="category"
-                            value={categoryS}
-                            {...register("category")}
-                            label="Categoria"
-                            onChange={handleSelectCategoryChange}
-                            error={!!errors.company}
-                            
-                            
-                        >
-                        {
-                            Category.map((category)=>{
-                            return (
-                                <MenuItem key={category.id} value={category.id} >{category.category} </MenuItem>
-                            );
-                            })
-                            
-                        }; 
-                        </Select>
-                    </FormControl>
+                                labelId="subCategory"
+                                id="subCategory"
+                                value={subCategoryS}
+                                {...register("subCategory")}
+                                label="Sub Categoria"
+                                onChange={handleSelectSubCategoryChange}
+                                error={!!errors.company}
+                                disabled={isDisableSubCategory}
+                            >
+                            {
+                                SubCategoryByCategory.map((subCategory)=>{
+                                return (
+                                    <MenuItem key={subCategory.id} value={subCategory.id}>{subCategory.subcategory} </MenuItem>
+                                );
+                                })
+                                
+                            }; 
+                            </Select>
+                        </FormControl>
+                </Grid>
+                <Grid item xs={4}>
+                <FormControl fullWidth>
+                            <InputLabel id="categoryCases">Tipo Caso </InputLabel>
+                            <Select
 
-                    
-                    
-              
+                                labelId="categoryCases"
+                                id="categoryCases"
+                                value={categoryCaseS}
+                                {...register("categoryCases")}
+                                label="Categoria"
+                                onChange={handleSelectCategoryCaseChange}
+                                error={!!errors.company}
+                                disabled={isDisableCategoryCase}
+                            >
+                            {
+                                CategoryCasesBySubCategory.map((categoryCase)=>{
+                                return (
+                                    <MenuItem key={categoryCase.id} value={categoryCase.id} >{categoryCase.title} </MenuItem>
+                                );
+                                })
+                                
+                            }; 
+                            </Select>
+                        </FormControl>
+                </Grid>
             </Grid>
 
-            <Grid item xs={4}>
-            <FormControl fullWidth>
-                        <InputLabel id="subCategory">SubCategoria</InputLabel>
-                        <Select
-
-                            labelId="subCategory"
-                            id="subCategory"
-                            value={subCategoryS}
-                            {...register("subCategory")}
-                            label="Sub Categoria"
-                            onChange={handleSelectSubCategoryChange}
-                            error={!!errors.company}
-                            disabled={isDisableSubCategory}
-                        >
-                        {
-                            SubCategoryByCategory.map((subCategory)=>{
-                            return (
-                                <MenuItem key={subCategory.id} value={subCategory.id}>{subCategory.subcategory} </MenuItem>
-                            );
-                            })
-                            
-                        }; 
-                        </Select>
-                    </FormControl>
-            </Grid>
-            <Grid item xs={4}>
-            <FormControl fullWidth>
-                        <InputLabel id="categoryCases">Tipo Caso </InputLabel>
-                        <Select
-
-                            labelId="categoryCases"
-                            id="categoryCases"
-                            value={categoryCaseS}
-                            {...register("categoryCases")}
-                            label="Categoria"
-                            onChange={handleSelectCategoryCaseChange}
-                            error={!!errors.company}
-                            disabled={isDisableCategoryCase}
-                        >
-                        {
-                            CategoryCasesBySubCategory.map((categoryCase)=>{
-                            return (
-                                <MenuItem key={categoryCase.id} value={categoryCase.id} >{categoryCase.title} </MenuItem>
-                            );
-                            })
-                            
-                        }; 
-                        </Select>
-                    </FormControl>
-            </Grid>
-        </Grid>
-
-</Box>
-);
+    </Box>
+    );
 
   return (
     <BasicModal
