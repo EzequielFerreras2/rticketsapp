cf0bca9bfff049f6e95a07f2194644e26f27e358
Ezequiel Ferrreras <52600831+EzequielFerreras2@users.noreply.github.com>
2023-02-17 08:37:20 -0400

    Changes.

--- src/components/DashBoard/UserDashboard/UserDashboard.jsx
+++ src/components/DashBoard/UserDashboard/UserDashboard.jsx
@@ -1,9 +1,29 @@
+import { Card, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
+import photo from '../../../img/contru.jpg'
 
 const UserDashboard = () => {
     return (
         <div>
-            <h1>User Dashboard</h1>
+            <br/>
+            <br/>
+            <br/>
+            <Typography variant='h1' sx={{color:'black'}}>
+                DashBoard
+            </Typography>
+            <Box>
+               <Grid
+               container
+               direction="row"
+               justifyContent="center"
+               alignItems="center"
+               >
+                    <Grid item>
+                        <img src={photo} alt="photoa" height={600}/>
+                    </Grid>
+               </Grid>
+            </Box>
         </div>
     );
 }
