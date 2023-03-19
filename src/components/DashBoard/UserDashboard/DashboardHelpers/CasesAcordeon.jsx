3206cdf734cbf3e4d40a7a4c6988a485fc0bad96
Ezequiel Ferrreras <Ezequielferreras2@gmail.com>
2023-03-18 21:38:43 -0400

    Changes

---
+++ src/components/DashBoard/UserDashboard/DashboardHelpers/CasesAcordeon.jsx
@@ -0,0 +1,24 @@
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
+import { GridExpandMoreIcon } from '@mui/x-data-grid'
+import React from 'react'
+
+const CasesAcordeon = ({Cases}) => {
+  return (
+    <div>
+        <Accordion  sx={{mt:2,mb:2 ,backgroundColor: "#eeeeee" }}>
+            <AccordionSummary
+            expandIcon={<GridExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            >
+                <Typography sx={{ fontSize: 16 }}><b>Caso:</b> </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <h1>dd</h1>
+            </AccordionDetails>
+        </Accordion>                             
+    </div>
+  )
+}
+
+export default CasesAcordeon
--- src/components/DashBoard/UserDashboard/UserDashboard.jsx
+++ src/components/DashBoard/UserDashboard/UserDashboard.jsx
@@ -1,26 +1,66 @@
-import { Grid, Typography } from '@mui/material';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Card, CardContent, Grid, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import React from 'react';
-import photo from '../../../img/contru.jpg'
+import React, { useEffect } from 'react';
+
+import { useAtuhStore } from '../../../store/auth/useAuthStore';
+import { useCasesStore } from '../../../store/cases/useCasesStore';
+import CasesAcordeon from './DashboardHelpers/CasesAcordeon';
 
 const UserDashboard = () => {
+
+const {cases, onGetCasesByUser}= useCasesStore();
+const {user}=useAtuhStore();
+
+useEffect(() => {
+    onGetCasesByUser(user.id);
+}, []);
+    
     return (
         <div>
             <br/>
             <br/>
-            <br/>
+         
             <Typography variant='h1' sx={{color:'black'}}>
                 DashBoard
             </Typography>
+            <br/>
             <Box>
                <Grid
                container
                direction="row"
                justifyContent="center"
                alignItems="center"
+               spacing={2}
                >
-                    <Grid item>
-                        <img src={photo} alt="photoa" height={600}/>
+                    <Grid item xs={1} md={6}>
+                        <Card sx={{ minWidth: 275,background: "#eeeeee" }}>
+                            <CardContent>
+                                <Typography variant="h5" component="div">
+                                Casos Resueltos Recientes
+                                </Typography>
+                                <CasesAcordeon Cases={cases}/>
+                            </CardContent>
+                            
+                        </Card>
+                    </Grid>
+                    <Grid item xs={1} md={6}>
+                    <Card sx={{ minWidth: 275 ,background: "#eeeeee"}}>
+                            <CardContent>
+                                <Typography variant="h5" comp2onent="div">
+                                    Casos en proceso
+                                </Typography>
+                                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                                adjective
+                                </Typography>
+                                <Typography variant="body2">
+                                well meaning and kindly.
+                                <br />
+                                {'"a benevolent smile"'}
+                                </Typography>
+                            </CardContent>
+                            
+                        </Card>
                     </Grid>
                </Grid>
             </Box>
